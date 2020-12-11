04b94a57e49ae9cc806d2a48679b1d4d6c71af8a
nastmi <natan.semrl@gmail.com>
2020-12-11 02:29:41 +0100

    Used lru_cache to shorten soluton considerably.

--- day10/adapter.py
+++ day10/adapter.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 adapters = [0]
 adapters.extend([int(item) for item in open("day10/input.txt").read().split("\n")])
 adapters.append(max(adapters)+3)
@@ -12,34 +13,47 @@ print(count[1] * count[3]-1)
 
 
 #part 2
-def follows(num, adap):
+def follows(num):
     followSet = set()
-    followSet.add(num + 1) if num+1 in adap else followSet.update(set())
-    followSet.add(num + 2) if num+2 in adap else followSet.update(set())
-    followSet.add(num + 3) if num+3 in adap else followSet.update(set())
+    followSet.add(num + 1) if num+1 in adapters else followSet.update(set())
+    followSet.add(num + 2) if num+2 in adapters else followSet.update(set())
+    followSet.add(num + 3) if num+3 in adapters else followSet.update(set())
     return followSet
 
-valids = {item:0 for item in adapters}
-def count_valids(num, adap):
-    cur_follow = follows(num, adap)
+@lru_cache(3)
+def count_valids(num):
+    cur_follow = follows(num)
     total = 0
     if(len(cur_follow) == 0):
         return 1
     for num1 in cur_follow:
-        total += count_valids(num1, adap)
+        total += count_valids(num1)
     return total
 
-sep = []
+
+print(count_valids(0))
+
+#Old solution, before lru_cache. Seperates the list of adapters into mutliple smaller lists, where n-(n-1) = 3. 
+
+"""def follows(num, adap):
+    followSet = set()
+    followSet.add(num + 1) if num+1 in adap else followSet.update(set())
+    followSet.add(num + 2) if num+2 in adap else followSet.update(set())
+    followSet.add(num + 3) if num+3 in adap else followSet.update(set())
+    return followSet"""
+
+"""sep = []
 seqStart = 0
 for i in range(1, len(adapters)):
     if (adapters[i] - adapters[i-1]) == 3 or (adapters[i-1] - adapters[i]) == 3:
         sep.append(adapters[seqStart:i])
         seqStart = i
+
 print(sep)    
 for item in sep:
     total = total*count_valids(item[0], item)
     print(count_valids(item[0], item))
-print(total)
+print(total)"""
 
 
 
